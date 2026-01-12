a0d210d906b51611c0360c8451ff8e2cf45bbeb0
paprikanotfound <stavangr@icloud.com>
2026-01-12 20:26:35 +0000

    fix wrong import

--- src/routes/return/+page.svelte
+++ src/routes/return/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import { APP_CONFIG } from "$lib";
-	import { clearPersistedPage } from "$lib/components/canvas/persisted-page.svelte";
+	import { APP_CONFIG, clearPersistedScribble } from "$lib";
   
   // reset canvas on return
-  clearPersistedPage(APP_CONFIG.scribble.persist_front);
-  clearPersistedPage(APP_CONFIG.scribble.persist_back);
+  clearPersistedScribble(APP_CONFIG.scribble.persist_front);
+  clearPersistedScribble(APP_CONFIG.scribble.persist_back);
 </script>
 
 <div class="p-4 flex flex-col gap-4">
